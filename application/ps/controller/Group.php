db11237391986f5beb3806f6fd452bf8e4cf3386
hooker <y1-hooker@ik8s.com>
2020-08-30 15:25:51 +0800

    debug

--- application/ps/controller/Group.php
+++ application/ps/controller/Group.php
@@ -56,27 +56,61 @@ class Group extends Controller
     //全部图层数据
     public function layers()
     {
-        $canalMdl = new \app\ps\model\Canal();
-        $arrCanal = $canalMdl->field('gid,name')->select()->toArray();
-        $combMdl = new \app\ps\model\Comb();
-        $arrComb = $combMdl->field('gid,name')->select()->toArray();
-        $dpMdl = new \app\ps\model\Dirpoint();
-        $arrDp = $dpMdl->field('gid,name')->select()->toArray();
-        $pipeMdl = new \app\ps\model\Pipe();
-        $arrPipe = $pipeMdl->field('gid,name')->select()->toArray();
-        $spoutMdl = new \app\ps\model\Spout();
-        $arrSpout = $spoutMdl->field('gid,river as name')->select()->toArray();
-        $wellMdl = new \app\ps\model\Well();
-        $arrWell = $wellMdl->field('gid,name')->select()->toArray();
+//        $canalMdl = new \app\ps\model\Canal();
+//        $arrCanal = $canalMdl->field('gid,name')->select()->toArray();
+//        $combMdl = new \app\ps\model\Comb();
+//        $arrComb = $combMdl->field('gid,name')->select()->toArray();
+//        $dpMdl = new \app\ps\model\Dirpoint();
+//        $arrDp = $dpMdl->field('gid,name')->select()->toArray();
+//        $pipeMdl = new \app\ps\model\Pipe();
+//        $arrPipe = $pipeMdl->field('gid,name')->select()->toArray();
+//        $spoutMdl = new \app\ps\model\Spout();
+//        $arrSpout = $spoutMdl->field('gid,river as name')->select()->toArray();
+//        $wellMdl = new \app\ps\model\Well();
+//        $arrWell = $wellMdl->field('gid,name')->select()->toArray();
+//
+//        $data = [
+//            'canal' => ['type' => 1, 'result' => []],
+//            'comb' => ['type' => 2, 'result' => []],
+//            'dir_point' => ['type' => 3, 'result' => []],
+//            'pipe' => ['type' => 4, 'result' => []],
+//            'spout' => ['type' => 5, 'result' => []],
+//            'well' => ['type' => 6, 'result' => []],
+//        ];
+        $type = intval($this->request->get('type'));
+        $data = [];
+        switch ($type) {
+            case 1:
+                $data = [
+                    ['type' => 1, 'name' => '排水沟渠'],
+                    ['type' => 2, 'name' => '雨水口'],
+                    ['type' => 3, 'name' => '管线暗点'],
+                    ['type' => 4, 'name' => '排水管道'],
+                    ['type' => 5, 'name' => '排放口'],
+                    ['type' => 6, 'name' => '窖井'],
+                ];
+                break;
+            case 2:
+                $data = [
+                    ['type' => 7, 'name' => '排水沟渠雨水'],
+                    ['type' => 8, 'name' => '排水管道污水'],
+                    ['type' => 9, 'name' => '排水管道雨水'],
+                    ['type' => 10, 'name' => '排水管道雨污合流'],
+                ];
+                break;
+            case 3:
+                $data = [
+                    ['type' => 11, 'name' => '排水沟渠支管'],
+                    ['type' => 12, 'name' => '排水沟渠支管以下'],
+                    ['type' => 13, 'name' => '排水沟渠支干管'],
+                    ['type' => 14, 'name' => '排水管道支管'],
+                    ['type' => 15, 'name' => '排水管道支管以下'],
+                    ['type' => 16, 'name' => '排水管道支干管'],
+                    ['type' => 17, 'name' => '排水管道主干管'],
+                ];
+                break;
+        }
 
-        $data = [
-            'canal' => ['type' => 1, 'result' => $arrCanal],
-            'comb' => ['type' => 2, 'result' => $arrComb],
-            'dir_point' => ['type' => 3, 'result' => $arrDp],
-            'pipe' => ['type' => 4, 'result' => $arrPipe],
-            'spout' => ['type' => 5, 'result' => $arrSpout],
-            'well' => ['type' => 6, 'result' => $arrWell],
-        ];
         return jsonSuc($data);
     }
 }
