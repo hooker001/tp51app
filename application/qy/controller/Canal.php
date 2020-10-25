3d0d82eb16cb90c9712be1144d055ad67282d6fb
hooker <y1-hooker@ik8s.com>
2020-10-25 09:56:49 +0800

    qy canal

---
+++ application/qy/controller/Canal.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace app\qy\controller;
+
+use think\Controller;
+//use app\ps\validate\Canal as Vd;
+use app\qy\model\Canal as Mdl;
+use think\Db;
+
+class Canal extends Controller
+{
+    protected $fcode = '060102';
+
+    public function create()
+    {
+        $arrPost = $this->request->post();
+        if (!$arrPost) {
+            return jsonErr('缺失入参');
+        }
+//        $validate = new Vd();
+//        if (!$validate->check($arrPost)) {
+//            return jsonErr($validate->getError());
+//        }
+//        $usid = $this->fcode . '-' . $arrPost['code'] . '-';
+        $geom = $arrPost['geom'] ?? '';
+        unset($arrPost['geom']);
+//        $arrPost['fcode'] = $this->fcode;
+        if (!$arrPost) {
+            return jsonErr('项目名称必须填写');
+        }
+        $model = new Mdl();
+        $model->save($arrPost);
+        $id = $model->gid;
+//        $strId = str_pad($id, 6, '0', STR_PAD_LEFT);
+//        $usid .= $strId;
+        $sql = "update qy_canal_zy set geom=st_geomfromgeojson('$geom') where gid=" . $id;
+        Db::execute($sql);
+        return jsonSuc(['id' => $id]);
+    }
+
+    public function delete()
+    {
+        $id = intval($this->request->get('id'));
+        $pipe = Mdl::get($id);
+        if (!$pipe) {
+            return jsonErr('数据不存在');
+        }
+        $pipe->delete();
+        return jsonSuc();
+    }
+
+    public function info()
+    {
+        $id = intval($this->request->get('id'));
+        $info = Mdl::where('gid', $id)->field('*,st_asgeojson(geom) as geojson')->find();
+        if (!$info) {
+            return jsonErr('数据不存在');
+        }
+        return jsonSuc($info->toArray());
+    }
+
+    public function update()
+    {
+        $id = intval($this->request->get('id'));
+        $info = Mdl::get($id);
+        if (!$info) {
+            return jsonErr('数据不存在');
+        }
+        $arrPost = $this->request->post();
+        $data = $arrPost;
+//        if (isset($arrPost['code']) && $arrPost['code']) {
+//            $data['usid'] = $this->fcode . '-' . $arrPost['code'] . '-' . str_pad($id, 6, '0', STR_PAD_LEFT);
+//            unset($data['code']);
+//        }
+        $geom = '';
+        if (isset($arrPost['geom']) && $arrPost['geom']) {
+            $geom = $arrPost['geom'];
+            unset($data['geom']);
+        }
+        $mdl = new Mdl();
+        $mdl->save($data, ['gid' => $id]);
+        if ($geom) {
+            $sql = "update qy_canal_zy set geom=st_geomfromgeojson('$geom') where gid=" . $id;
+            Db::execute($sql);
+        }
+        return jsonSuc();
+    }
+}
---
+++ application/qy/model/Canal.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace app\qy\model;
+
+use think\Model;
+
+class Canal extends Model
+{
+    protected $pk = 'gid';
+
+    protected $table = 'qy_canal_zy';
+
+    public function getAll($arrCond)
+    {
+//        $allowField = ['district', 'subtype', 'lane_way', 'grade', 'sort', 'srv', 'geom', 'type'];
+        $arrParam = $this->checkCond($arrCond);
+        if (!$arrParam) {
+            return [];
+        }
+        $model = new self();
+        $model = $model->field('gid, layers, st_asgeojson(geom) as geom');
+        if (isset($arrParam['district']) && $arrParam['district']) {
+            $model = $model->where('district', 'in', $arrParam['district']);
+        }
+//        if (isset($arrParam['subtype']) && $arrParam['subtype']) {
+//            $model = $model->where('subtype', 'like', '%' . $arrParam['subtype'] . '%');
+//        }
+        if (isset($arrParam['lane_way']) && $arrParam['lane_way']) {
+            $model = $model->where('lane_way', 'in', $arrParam['lane_way']);
+        }
+        if (isset($arrParam['grade']) && $arrParam['grade']) {
+            $model = $model->where('grade', 'in', $arrParam['grade']);
+        }
+        if (isset($arrParam['sort']) && $arrParam['sort']) {
+            $model = $model->where('sort', 'in', $arrParam['sort']);
+        }
+        if (isset($arrParam['srv']) && $arrParam['srv']) {
+            $model = $model->where('srv', 'like', '%' . $arrParam['srv'] . '%');
+        }
+        if (isset($arrParam['geom']) && $arrParam['geom']) {
+            $model = $model->where("st_contains(st_geomfromgeojson('" . $arrParam['geom'] . "'),geom)");
+        }
+        if (isset($arrParam['option']) && $arrParam['option']) {
+            foreach ($arrParam['option'] as $item) {
+                if (!isset($item['f']) || !$item['f'] || !isset($item['o']) || !$item['o'] || !isset($item['v']) || !$item['v']) {
+                    continue;
+                }
+                $val = $item['o'] == 'like' ? '%' . $item['v'] . '%' : $item['v'];
+                if (isset($item['r']) && $item['r'] == 'or') {
+                    $model = $model->whereOr($item['f'], $item['o'], $val);
+                } else {
+                    $model = $model->where($item['f'], $item['o'], $val);
+                }
+            }
+        }
+        return $model->order('gid', 'desc')->select()->toArray();
+    }
+
+    //如果存在不属于当前模型的字段，则验证失败
+    protected function checkCond($arrCond)
+    {
+        $fields = $this->getTableFields();
+        $unkownField = false;
+        foreach ($arrCond as $f => $v) {
+            if ($f == 'option') {
+                foreach ($v as $item) {
+                    if (!in_array($item['f'], $fields)) {
+                        $unkownField = true;
+                        break 2;
+                    }
+                }
+            } else {
+                if (!in_array($f, $fields)) {
+                    $unkownField = true;
+                    break;
+                }
+            }
+        }
+        return $unkownField ? false : $arrCond;
+    }
+}
